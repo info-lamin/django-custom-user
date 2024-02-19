0d1cdecff15b04f5705e8ec2d430625e3f957fec
Lamin Muhammed M <laminmuhammed@gmail.com>
2024-02-19 18:44:54 +0530

    registered admin for user using forms

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,37 @@
+from .models import User
 from django.contrib import admin
+from .forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = (
+        "email", "first_name", "last_name", "is_staff")
+    list_filter = (
+        "is_staff", "is_superuser", "is_active", "groups")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": (
+            "first_name", "last_name")}),
+        ("Permissions", {"fields": (
+            "is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": (
+            "last_login", "date_joined")})
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2"
+            )}),
+    )
+    search_fields = ("email")
+    ordering = ("email")
+
+
+admin.site.register(User, UserAdmin)
